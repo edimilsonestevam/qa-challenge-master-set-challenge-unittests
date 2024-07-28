dd35fb490d4043f1dc5908289036dcc9daa5ceaf
edimilsonestevam <edimilsonestevam@gmail.com>
2024-07-28 16:44:16 -0600

    Technical Challenges
    I am adding new test project.

---
+++ qa-challenge-master-set-challenge-unittests/set-challenge/unittests/netsim-feedzai/src/test/java/netsim/NetworkPathTest.java
@@ -0,0 +1,127 @@
+package netsim;
+
+import static netsim.NetworkUtils.B;
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.feedzai.netsim.engine.Network;
+import com.feedzai.netsim.engine.NetworkPath;
+
+public class NetworkPathTest {
+	private final static Logger LOGGER = Logger.getLogger(NetworkPathTest.class.getName());
+	private Network net;
+	private int defaultLatency = 1;
+
+	/**
+	 * Setup routine that is executed before each test case.
+	 */
+	@Before
+	public void setUp() throws Exception {
+		LOGGER.info("Prepare setup for my test {}.");
+	}
+
+
+	@Test
+	public void testNetworkPathFastestRoute() {
+		net = Network.createWithLatency(defaultLatency);
+		net.connect("A", "B", 1);
+		net.connect("B", "C", 1);
+		net.connect("A", "C", 3);
+		NetworkPath path = net.sendPacket("A", "C");
+				
+		assertEquals("Wrong expected latency between nodes - ", 2, path.getTime());
+		assertEquals("Wrong path.", path.toString(), "[A,B,C]");
+	}
+
+	@Test
+	public void testNetworkPathShortestPath() {
+		net = Network.createWithLatency(defaultLatency);
+		net.connect("A", "C", 2);
+		net.connect("A", "B", 1);
+		net.connect("B", "C", 1);
+		NetworkPath path = net.sendPacket("A", "C");
+	}
+
+	@Test
+	public void testNetworkPathTwoRoutesPossibleInCircle() {
+		net = Network.createWithLatency(defaultLatency);
+		net.connect("A", "B");
+		net.connect("B", "C");
+		net.connect("C", "D");
+		net.connect("D", "A");
+		NetworkPath path = net.sendPacket("A", "C");
+		
+		if(path.contains("B")) {
+			assertEquals("Wrong path.", path.toString(), "[A,B,C]");
+			assertFalse("Path contains wrong node", path.contains("D"));
+		} else {
+			assertEquals("Wrong path.", path.toString(), "[A,D,C]");
+			assertFalse("Path contains wrong node", path.contains("B"));
+		}
+		assertEquals("Wrong expected latency between nodes - ", 2, path.getTime());
+		
+	}
+
+	@Test
+	public final void testSendPacketSameNodesOppositeDirection() {
+		// Simple case that tests bidirectionality of connections.
+		net = Network.createWithLatency(defaultLatency);
+		net.connect("A", B);
+		NetworkPath path = net.sendPacket("A", "B");
+		NetworkPath path2 = net.sendPacket("B", "A");
+
+		assertEquals("Wrong expected latency between nodes - ", defaultLatency, path.getTime());
+		assertEquals("Wrong number of nodes in path", 2, path.size());
+		assertEquals("Start node is not the origin node", path.peekFirst(), "A");
+		assertEquals("Final node is not the destination node", path.peekLast(), "B");
+		assertEquals("Wrong expected latency between nodes - ", defaultLatency, path2.getTime());
+		assertEquals("Wrong number of nodes in path", 2, path2.size());
+		assertEquals("Start node is not the origin node", path2.peekFirst(), "B");
+		assertEquals("Final node is not the destination node", path2.peekLast(), "A");
+
+	}
+
+	@Test
+	public final void testSendPacketSameNodesWithDiffLatencies() {
+		// Corner case that tests bidirectionality of connections with different latencies.
+		net = Network.createWithLatency(defaultLatency);
+		int smallLatency = 2;
+		int bigLatency = 3;
+		net.connect("A", "B", smallLatency);
+		net.connect("B", "A", bigLatency);
+		NetworkPath path = net.sendPacket("A", "B");
+		NetworkPath path2 = net.sendPacket("B", "A");
+
+		assertEquals("Wrong expected latency between nodes - ", smallLatency, path.getTime());
+		assertEquals("Wrong number of nodes in path", 2, path.size());
+		assertEquals("Start node is not the origin node", path.peekFirst(), "A");
+		assertEquals("Final node is not the destination node", path.peekLast(), "B");
+		assertEquals("Wrong expected latency between nodes - ", smallLatency, path2.getTime());
+		assertEquals("Wrong number of nodes in path", 2, path2.size());
+		assertEquals("Start node is not the origin node", path2.peekFirst(), "B");
+		assertEquals("Final node is not the destination node", path2.peekLast(), "A");
+	}
+
+	public void testNetworkPathRandomNetwork() {
+		for(int i = 0; i < 100; i++) {
+			Network net = NetworkUtils.createRandomNetwork(1000, 1000, defaultLatency, "NodeStart", "NodeFinish");
+			NetworkPath path = net.sendPacket("NodeStart", "NodeFinish");
+
+			if(path.getTime() > 0) {
+				assertEquals("Start node is not the origin node", path.peekFirst(), "NodeStart");
+				assertEquals("Final node is not the destination node", path.peekLast(), "NodeFinish");
+				assertTrue(path.size()==path.getTime()+1);
+				for(String node : path) {
+					assertTrue(Collections.frequency(path, node)==1);
+				}
+				//System.out.println(path);
+			}
+		}
+	}
+}
---
+++ qa-challenge-master-set-challenge-unittests/set-challenge/unittests/netsim-feedzai/src/test/java/netsim/NetworkUtils.java
@@ -0,0 +1,58 @@
+package netsim;
+
+import com.feedzai.netsim.engine.Network;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class NetworkUtils {
+    static String B = getIntFromPath() % 2 == 0 ? "B" : "A";
+
+    private NetworkUtils() {}
+
+    /**
+     * Auxiliary method that creates a random network with a specific amount of connections, that are randomly chosen from a defined pool of nodes.
+     * We do not ensure that there isn't repeated connections, however we assure that there isn't a connection from/to the same node.
+     * We can also set the default latency between each connection and provide 2 specific nodes to be part of the network.
+     * (Those 2 nodes should be used in the network as the origin/destination node)
+     * @param numberConnections - Number of connections of the network
+     * @param uniqueNodes - Pool size of unique nodes to be randomly picked
+     * @param latency - Default latency for each connection
+     * @param originNode - 1st node that we're 100% sure that will be part of the network
+     * @param destinationNode - 2nd node that we're 100% sure that will be part of the network
+     * @return
+     */
+    public static Network createRandomNetwork(int numberConnections, int uniqueNodes, int latency, String originNode, String destinationNode) {
+        Network network = Network.createWithLatency(latency);
+        network.connect(originNode, getRandomString(uniqueNodes));
+
+        for(int i = 0; i < numberConnections; i++) {
+            String leftNode = getRandomString(uniqueNodes);
+            String rightNode = getRandomString(uniqueNodes);
+
+            while(leftNode.equals(rightNode)) {
+                rightNode = getRandomString(uniqueNodes);
+            }
+            network.connect(leftNode, rightNode);
+        }
+        network.connect(getRandomString(uniqueNodes), destinationNode);
+        return network;
+    }
+
+    /**
+     * Auxiliary method that generates a string with pattern "NodeXXXX", with the max value of XXXX being an argument of this method.
+     * @param chances - Max value of XXXX
+     * @return String with pattern "NodeXXXX"
+     */
+    private static String getRandomString(int chances) {
+        Random r = new Random();
+        return "Node"+(r.nextInt(chances)+1);
+    }
+
+    private static int getIntFromPath() {
+        Random rand = new Random();
+        List<Integer> givenList = Arrays.asList(2, 4, 6, 3);
+        return givenList.get(rand.nextInt(givenList.size()));
+    }
+}
